22c182bdc0b2080b93e12df8feb55eff16cef573
Denis <krollik@gmail.com>
2020-02-19 11:13:33 +0200

    redux

--- lesson2/task3/src/index.js
+++ lesson2/task3/src/index.js
@@ -4,5 +4,8 @@ import  store from './store.js';
 
 store.dispatch(add({id: 76, name: 'Sarah'}));
 store.dispatch(add({id: 85, name: 'Denis'}));
+store.dispatch(add({id: 54, name: 'Oleg'}));
+store.dispatch(add({id: 43, name: 'Koly'}));
 store.dispatch(delet(76));
+store.dispatch(delet(54));
 console.log(store.getState());
