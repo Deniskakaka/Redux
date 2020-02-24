6e76dc66180d3c8a9d0fe838085ee323abc98f31
Denis <krollik1000000@gmail.com>
2020-02-24 13:04:12 +0200

    redux

--- lesson3/task1/src/users.actions.js
+++ lesson3/task1/src/users.actions.js
@@ -1,32 +1,25 @@
 export const ADD_USER = 'USERS/ADD_USER';
-export const DELETE_USER = 'USERS/DELETE_USER';
-export const UPDATE_USER = 'USER/UPDATE_USER';
+export const ADD_USER = 'ADD_USER';
+export const DELETE_USER = 'DELETE_USER';
+export const UPDATE_USER = 'UPDATE_USER';
 
-
-export const addUser = (userData) => {
-    return {
-        type:ADD_USER,
-       payLoad: {
-            userData
-        }
-    };
+export function addUser(userData) {
+  return {
+    type: ADD_USER,
+    payload: { userData }
+  };
 };
 
-export const deleteUser = (userId) => {
-    return {
-        type:DELETE_USER,
-        payLoad: {
-            userId
-        }
-    };
+export function deleteUser(userId) {
+  return {
+    type: DELETE_USER,
+    payload: { userId }
+  };
 };
 
-export const updateUser = (userId, userData) => {
-    return {
-        type:UPDATE_USER,
-        payLoad: {
-            userId,
-            userData
-        }
-    };
+export function updateUser(userId, userData) {
+  return {
+    type: UPDATE_USER,
+    payload: { userId, userData }
+  };
 };
