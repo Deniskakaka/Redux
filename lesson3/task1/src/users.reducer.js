c5ba15f931d3a11158a9a9d574e444a9f7998b44
Denis <krollik1000000@gmail.com>
2020-02-24 13:00:59 +0200

    redux

--- lesson3/task1/src/users.reducer.js
+++ lesson3/task1/src/users.reducer.js
@@ -6,21 +6,18 @@ const initialState = {
 
 const usersReducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_USER : {
+        case ADD_USER : 
             return {
                 ...state,
                 usersList: state.usersList.concat(action.payLoad.userData)
             }
-        }
-        case DELETE_USER : {
-            const newList = state.usersList
-            .filter(user => user.id !== action.payLoad.userId);
+        case DELETE_USER : 
+            const newList = state.usersList.filter(user => user.id !== action.payLoad.userId);
             return {
                 ...state,
                 usersList:newList
             }
-        }
-        case UPDATE_USER : {
+        case UPDATE_USER : 
             const newList = state.usersList.map(user => {
                 if (user.id === action.payLoad.userId) {
                    return {
@@ -33,7 +30,6 @@ const usersReducer = (state = initialState, action) => {
                 ...state,
                 usersList: newList
             }
-        }
         default:
             return state;
     }
