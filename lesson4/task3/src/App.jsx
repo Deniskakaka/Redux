2ccadd1fdfeca620b4d91c65d911d6c64d055b5d
Denis <krollik1000000@gmail.com>
2020-02-25 21:08:33 +0200

    redux

--- lesson4/task3/src/App.jsx
+++ lesson4/task3/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store.js';
-import UserList from './users/UserLIst.jsx';
+import UsersList from './users/UsersList.jsx';
 
 const App = () => {
     return (
         <Provider store={store}>
-            <UserList/>
+            <UsersList/>
         </Provider>
     );
 };
