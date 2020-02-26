ce39c0538fe711a5db096af9946c8403fdaca610
Denis <krollik1000000@gmail.com>
2020-02-26 17:21:44 +0200

    redux

--- lesson4/task3/src/users/Pagination.jsx
+++ lesson4/task3/src/users/Pagination.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 const Pagination = (props) => {
     const {currentPage, itemsPerPage,goPrev,goNext,totalItems} = props
-       const max =  currentPage + 1 > Math.ceil(totalItems / itemsPerPage) - 1
+    const min = currentPage === 0;
+    const max =  currentPage + 1 > Math.ceil(totalItems / itemsPerPage) - 1
     
     return(
             <div className="pagination">
                 <button 
                     className="btn" onClick={goPrev} 
-                    disabled={currentPage === 0 ? true: false}>
-                    {currentPage === 0 ? "": '←'}
+                    disabled={min}>
+                    {min ? "": '←'}
                 </button>
                 <span className="pagination__page">{currentPage}</span>
                 <button 
